11cdb26ff46a5452b0b196a1d048927d4383d33b
Ahulo Sunday <ahulosunday@yahoo.com>
2023-08-06 20:08:53 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -145,16 +145,20 @@ router.get('/lookups/:id/region',lookup.lookUpRegion)
 router.get('/lookups/:id/country',lookup.lookUpCountry)
 /**/
 //===============image function=========
-const pathImg ="https://aisnet.onrender.com/"
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb){
-    cb(null, pathImg + 'upload')
+    cb(null, '../clients/public/upload')
   },
   filename: function (req, file, cb){
     cb(null, Date.now() + file.originalname)
   },
 });
-const upload = multer({storage})
+const upload = multer({
+    storage,
+    limits:{fieldSize:1000000},
+    
+    })
 router.post('/uploadfile', upload.single('file'), function(req, res){
   const file = req.file
   return res.status(200).json(file.filename)
